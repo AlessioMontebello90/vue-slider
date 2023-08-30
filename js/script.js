996d61c8d1d83e97ce32eb9a0b72c3a3bd8ca477
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-08-30 19:28:57 +0200

    fix JS

--- js/script.js
+++ js/script.js
@@ -31,6 +31,11 @@ createApp({
           text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
         },
       ],
+
+      activeImg: 0,
+      autoPlay: true,
     };
   },
+
+  methods: {},
 }).mount("#app");
